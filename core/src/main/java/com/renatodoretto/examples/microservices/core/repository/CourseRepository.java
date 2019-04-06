da3755b4ae92ef587557a794f76e648f36dc6afe
Renato Doretto <rnt.doretto@gmail.com>
2019-04-06 18:06:20 -0300

    Limpeza de branch

--- core/src/main/java/com/renatodoretto/examples/microservices/core/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.renatodoretto.examples.microservices.core.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.renatodoretto.examples.microservices.core.model.Course;
-
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long>{
-	
-}
