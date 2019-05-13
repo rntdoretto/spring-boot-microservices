470ef10a37e733dc5d73ddef32012eecedc7bc01
Renato Doretto <rnt.doretto@gmail.com>
2019-05-12 21:44:46 -0300

    Added Price microservice

--- price/src/main/java/com/renatodoretto/examples/microservices/price/PriceApplication.java
+++ price/src/main/java/com/renatodoretto/examples/microservices/price/PriceApplication.java
@@ -2,8 +2,12 @@ package com.renatodoretto.examples.microservices.price;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = {"com.renatodoretto.examples.microservices.core.model"})
+@EnableJpaRepositories(basePackages = {"com.renatodoretto.examples.microservices.core.repository"})
+@SpringBootApplication(scanBasePackages = {"com.renatodoretto.examples.microservices"})
 public class PriceApplication {
 
 	public static void main(String[] args) {
--- price/src/main/resources/data.sql
+++ price/src/main/resources/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO Price(currency, price)
 VALUES('BRL', 199.99);
-INSERT INTO Course(title)
+INSERT INTO Price(currency, price)
 VALUES('USD', 399.99);
-INSERT INTO Course(title)
+INSERT INTO Price(currency, price)
 VALUES('EUR', 99.99);
-INSERT INTO Course(title)
+INSERT INTO Price(currency, price)
 VALUES('BRL', 699.99);
-INSERT INTO Course(title)
+INSERT INTO Price(currency, price)
 VALUES('BRL', 99.99);
-INSERT INTO Course(title)
+INSERT INTO Price(currency, price)
 VALUES('BRL', 399.99);
