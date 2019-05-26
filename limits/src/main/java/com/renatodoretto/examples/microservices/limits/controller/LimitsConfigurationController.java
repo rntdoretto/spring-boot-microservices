f7f34fb0268402282826696904fe69d6303d9de0
Renato Doretto <rnt.doretto@gmail.com>
2019-05-26 20:12:38 -0300

    All running final

--- limits/src/main/java/com/renatodoretto/examples/microservices/limits/controller/LimitsConfigurationController.java
+++ limits/src/main/java/com/renatodoretto/examples/microservices/limits/controller/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.renatodoretto.examples.microservices.limits.configuration.Configuration;
 import com.renatodoretto.examples.microservices.limits.model.LimitConfiguration;
 
@@ -16,5 +17,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/limits/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveLimitsConfigurations")
+	public LimitConfiguration retrieveLimitsConfigurations() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveLimitsConfigurations() {
+		return new LimitConfiguration(999, 9);
 	}
 }
